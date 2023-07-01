1b6ee7b0af3463e49e62ed7a05289a7b29f68dc1
Joseph Ofei <josephofei24@gmail.com>
2023-07-01 13:36:39 -0400

    add nav element to the navbar component

--- src/Component/OldNavBar.js
+++ src/Component/OldNavBar.js
@@ -8,11 +8,11 @@ function NavBar({ onChangePage }) {
     }
 
     return (
-        // <nav>
-        //     <a onClick={handleLinkClick} href="/">Home</a>
-        //     <a onClick={handleLinkClick} href="/form">Form</a>
-        //     <a onClick={handleLinkClick} href="/contact">Contact</a>
-        // </nav>
+        <nav>
+            <a onClick={handleLinkClick} href="/">Home</a>
+            <a onClick={handleLinkClick} href="/form">Form</a>
+            <a onClick={handleLinkClick} href="/contact">Contact</a>
+        </nav>
     );
 }
 
