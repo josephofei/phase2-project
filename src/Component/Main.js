6c8b70754bca798934ec2c826ce23d33ca231201
Joseph Ofei <josephofei24@gmail.com>
2023-07-01 13:35:26 -0400

    add prop to the main component

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Contacts from './Contacts'
 import Form from './Form'
 
-function Main({ contacts, handleSubmit }) {
+// function Main({ contacts, handleSubmit }) {
 
     return (
-        // <main>
-        //     {/* <Form handleSubmit={handleSubmit} /> */}
-        //     <Form />
-        //     <Contacts contacts={contacts} />
+        <main>
+            {/* <Form handleSubmit={handleSubmit} /> */}
+            <Form />
+            <Contacts contacts={contacts} />
 
-        // </main>
+        </main>
     )
 }
 
